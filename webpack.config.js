3272064c96621e9f99be38ea94eb4c6994b41abc
SirriCelles <sirricelles@gmail.com>
2022-06-10 02:05:39 +0100

    add html congfigs

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: '[name].js',
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Output Management',
+        template: path.resolve(__dirname, 'src', 'index.html')
+      })
+    ],
+  }
+}
