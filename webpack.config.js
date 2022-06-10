7657e9a11f7dbef4d47a1e2dd66736fb72b2a500
SirriCelles <sirricelles@gmail.com>
2022-06-10 02:14:11 +0100

    correct config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: {
     index: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: '[name].js',
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Output Management',
-        template: path.resolve(__dirname, 'src', 'index.html')
-      })
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-        }
-      ]
-    }
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: path.resolve(__dirname, 'src', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist')
   }
 }
